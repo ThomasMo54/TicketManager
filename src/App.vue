8db266a60294511ec2c85f38ea7c4a0dd65f9f99
ThomasMo54 <loupimotom@live.com>
2020-06-16 19:47:34 +0200

    alpha commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TopBar @inputData='redirect' :key="topBarKey"/>
+    <HomePage v-if="home" @inputData='redirect' :key='homeKey'/>
+    <LoginPage v-if="login" @inputData='loggedIn'/>
+    <AdminPage v-if="admin"/>
+    <AddTicketPage v-if="addTicket" @inputData='redirect'/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopBar from './components/TopBar.vue'
+import HomePage from './components/HomePage.vue'
+import LoginPage from './components/LoginPage.vue'
+import AdminPage from './components/AdminPage.vue'
+import AddTicketPage from './components/AddTicketPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopBar, HomePage, LoginPage, AdminPage, AddTicketPage
+  },
+  data(){
+    return {
+      home: true,
+      login: false,
+      admin: false,
+      addTicket: false,
+      topBarKey: 0,
+      homeKey: 0
+    }
+  },
+  methods: {
+    goHome: function() {
+      this.login = false;
+      this.admin = false;
+      this.addTicket = false;
+      this.home = true;
+      this.topBarKey += 1;
+    },
+
+    goLogin: function() {
+      this.home = false;
+      this.admin = false;
+      this.addTicket = false;
+      this.login = true;
+    },
+
+    goAdmin: function() {
+      this.home = false;
+      this.login = false;
+      this.addTicket = false;
+      this.admin = true;
+    },
+
+    goAddTicket: function() {
+      this.home = false;
+      this.login = false;
+      this.admin = false;
+      this.addTicket = true;
+    },
+
+    redirect: function(action) {
+      if(action === "home") {
+        this.goHome();
+      }
+      if(action === "login") {
+        this.goLogin();
+      }
+      if(action === "admin") {
+        this.goAdmin();
+      }
+      if(action === "addTicket") {
+        this.goAddTicket();
+      }
+      if(action === "logout") {
+        this.$session.destroy();
+        this.homeKey += 1;
+        this.goHome();
+      }
+    },
+
+    loggedIn: function(username) {
+      this.$store.state.username = username;
+      this.$store.state.loggedIn = true;
+
+      this.$session.start();
+      this.$session.set("username", username);
+
+      this.goHome();
+    }
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0px;
+  padding: 0px;
+
+  background: #fafafa;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
